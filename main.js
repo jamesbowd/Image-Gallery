d3d1711658b4be2b1147eae7ef21b64807c5df08
James Bowden <jamesbowd@gmail.com>
2021-03-21 20:12:10 +0000

    followed MDN example: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Image_gallery

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 1; i <= 5; i++) {
+  const newImage = document.createElement("img");
+  newImage.setAttribute("src", "/images/pic" + i + ".jpg");
+  thumbBar.appendChild(newImage);
+
+  newImage.addEventListener("click", function () {
+    const source = newImage.src;
+    displayedImage.setAttribute("src", source);
+  });
+}
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener("click", function () {
+  className = btn.getAttribute("class");
+
+  if (className === "dark") {
+    console.log(btn.className);
+    btn.setAttribute("class", "light");
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+  } else if (this.className === "light") {
+    console.log(btn.className);
+    btn.setAttribute("class", "dark");
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgba(0,0,0,0)";
+  }
+});
